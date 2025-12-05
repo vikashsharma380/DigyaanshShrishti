82df87ab0cc0ddf20f4326884b53f3132a92f202
Vikash Sharma <vikashpandit380@gmail.com>
2025-12-05 17:23:04 +0530

    first commit

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -11,5 +11,22 @@ router.post("/create", async (req, res) => {
     res.json({ success: false, error: err.message });
   }
 });
+router.get("/list", async (req, res) => {
+  try {
+    const list = await Contact.find().sort({ createdAt: -1 });
+    res.json({ success: true, list });
+  } catch (err) {
+    res.json({ success: false, error: err.message });
+  }
+});
 
+// DELETE MESSAGE
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    await Contact.findByIdAndDelete(req.params.id);
+    res.json({ success: true });
+  } catch (err) {
+    res.json({ success: false, error: err.message });
+  }
+});
 export default router;
