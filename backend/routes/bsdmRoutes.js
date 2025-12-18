0b8eef9e461b658930211c7dc888a56815121fc2
Vikash Sharma <vikashpandit380@gmail.com>
2025-12-18 15:14:11 +0530

    removed .env from tracking

--- backend/routes/bsdmRoutes.js
+++ backend/routes/bsdmRoutes.js
@@ -3,20 +3,18 @@ import upload from "../middleware/uploadS3.js";
 import BsdmPage from "../models/BsdmPage.js";
 
 const router = express.Router();
-
 // GET page data
-router.get("/bsdm", async (req, res) => {
+router.get("/", async (req, res) => {
   const page = await BsdmPage.findOne();
   res.json(page);
 });
 
-
-// UPDATE images
+// ADD image
 router.post(
-  "/bsdm/add-image",
+  "/add-image",
   upload.single("image"),
   async (req, res) => {
-    const page = await BsdmPage.findOne() || new BsdmPage();
+    const page = (await BsdmPage.findOne()) || new BsdmPage();
 
     page.heroImages.push(req.file.location);
     await page.save();
@@ -24,7 +22,9 @@ router.post(
     res.json({ success: true, images: page.heroImages });
   }
 );
-router.delete("/bsdm/remove-image", async (req, res) => {
+
+// REMOVE image
+router.delete("/remove-image", async (req, res) => {
   const { imageUrl } = req.body;
 
   const page = await BsdmPage.findOne();
