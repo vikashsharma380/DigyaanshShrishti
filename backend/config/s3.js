9a1aae9651e54896d61d26eaeb7ad6f99b3c82a6
Vikash Sharma <vikashpandit380@gmail.com>
2025-12-18 13:53:45 +0530

    removed .env from tracking

--- backend/config/s3.js
+++ backend/config/s3.js
@@ -1,10 +1,10 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION,
-  
+  accessKeyId: AKIA4H3HQPH77EQFMUIQ,
+  secretAccessKey: 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,
+  region: eu-north-1,
+
 });
 
 const s3 = new AWS.S3();
--- backend/middleware/uploadS3.js
+++ backend/middleware/uploadS3.js
@@ -2,14 +2,12 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import s3 from "../config/s3.js";
 
-console.log("UPLOAD ENV CHECK:");
-console.log("AWS_BUCKET_NAME =", process.env.AWS_BUCKET_NAME);
-console.log("AWS_REGION =", process.env.AWS_REGION);
+
 
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.AWS_BUCKET_NAME,
+    bucket: digyaansh-bsdm-images,
     acl: "public-read",
     key(req, file, cb) {
       cb(null, `bsdm/${Date.now()}-${file.originalname}`);
